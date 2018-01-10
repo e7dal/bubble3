83012fe8dbd589cd63ee1460463937cf82300b30
Erdal Yildiz <e7dal@users.noreply.github.com>
2018-01-10 10:32:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     from distutils.core import setup
 
 
-readme = open('README.rst').read()
+readme = open('README.md').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 curr_path = os.path.dirname(os.path.realpath(__file__))
 deps = os.path.join(curr_path, 'requirements.txt')
@@ -25,21 +25,6 @@ metadata = imp.load_source(
 
 #orderdict needed for structlog
 sys_version_str='.'.join((str(s) for s in sys.version_info[0:3]))
-
-if sys_version_str == '2.6.9':
-    #on 2.6.9 ssl will not work so pip will not work also :(
-    #totally depracated but luckily easy_install will still kind of work
-    #https://pypi.python.org/pypi/setuptools
-    #os.system('wget https://bootstrap.pypa.io/ez_setup.py -O - | python')
-    #os.system('easy_install importlib') #drop in replacement from 2.7
-    #os.system('easy_install ordereddict') #backported from PY3
-    #in issue 9: easy_install will be mentioned as an alternative
-    
-    print('BUBBLE setup.by: 2.6.9: installing ordereddict and importlib')
-    print('BUBBLE setup.by: 2.6.9: https://github.com/realopenit/bubble/issues/9 for more info')
-    os.system('pip install importlib') #backported from 2.7
-    os.system('pip install ordereddict') #drop in replacement from 2.7
-    
 
 
 setup(
@@ -73,9 +58,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: POSIX :: Linux',
         'Environment :: Console',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements,
